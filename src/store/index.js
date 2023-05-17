4f1cae03df1a95e004549c4d634fef5630971589
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-17 09:32:30 -0300

    adicionei o saga de carrinhoSaga e o slice de usuario

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,11 @@ import categoriasSlice from './reducers/categorias';
 import itensSlice from './reducers/itens';
 import carrinhoSlice from './reducers/carrinho';
 import buscaSlice from './reducers/busca';
-import { categoriasListener } from './middlewares/categorias';
 import { itensListener } from './middlewares/itens';
 import createSagaMiddleware from 'redux-saga';
 import { categoriasSaga } from './sagas/categorias';
+import { carrinhoSaga } from './sagas/carrinho';
+import usuarioSlice from './reducers/usuario';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,15 +17,13 @@ const store = configureStore({
     itens: itensSlice,
     carrinho: carrinhoSlice,
     busca: buscaSlice,
+    usuario: usuarioSlice,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().prepend(
-      // categoriasListener.middleware,
-      itensListener.middleware,
-      sagaMiddleware
-    ),
+    getDefaultMiddleware().prepend(itensListener.middleware, sagaMiddleware),
 });
 
 sagaMiddleware.run(categoriasSaga);
+sagaMiddleware.run(carrinhoSaga);
 
 export default store;
