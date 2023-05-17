3f67147e8ff0c3f84075435c959140f5bc06f2c9
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-17 09:29:32 -0300

    removi as mudanças que tinham no total e mudei o state.carrinho para state.carrinho.data

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -8,23 +8,25 @@ import { useNavigate } from 'react-router-dom';
 export default function Carrinho() {
   const navigate = useNavigate();
 
-  const { carrinho, total } = useSelector(state => {
+  const { carrinho, total } = useSelector((state) => {
     let total = 0;
     const regexp = new RegExp(state.busca, 'i');
-    const carrinhoReduce = state.carrinho.reduce((itens, itemNoCarrinho) => {
-      const item = state.itens.find(item => item.id === itemNoCarrinho.id);
-      total += (item.preco * itemNoCarrinho.quantidade);
-      if (item.titulo.match(regexp)) {
-        itens.push({
-          ...item,
-          quantidade: itemNoCarrinho.quantidade,
-        });
-      }
-      return itens;
-    }, []);
+    const carrinhoReduce = state.carrinho.data.reduce(
+      (itens, itemNoCarrinho) => {
+        const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
+        if (item.titulo.match(regexp)) {
+          itens.push({
+            ...item,
+            quantidade: itemNoCarrinho.quantidade,
+          });
+        }
+        return itens;
+      },
+      []
+    );
     return {
       carrinho: carrinhoReduce,
-      total,
+      total: state.carrinho.total,
     };
   });
   return (
@@ -34,19 +36,17 @@ export default function Carrinho() {
         descricao='Confira produtos que você adicionou ao carrinho.'
       />
       <div className={styles.carrinho}>
-        {carrinho.map(item => <Item key={item.id} {...item} carrinho />)}
+        {carrinho.map((item) => (
+          <Item key={item.id} {...item} carrinho />
+        ))}
         <div className={styles.total}>
-          <strong>
-            Resumo da compra
-          </strong>
+          <strong>Resumo da compra</strong>
           <span>
             Subtotal: <strong> R$ {total.toFixed(2)} </strong>
           </span>
         </div>
-        <Button onClick={() => navigate('/pagamento')}>
-          Finalizar compra
-        </Button>
+        <Button onClick={() => navigate('/pagamento')}>Finalizar compra</Button>
       </div>
     </div>
-  )
+  );
 }
